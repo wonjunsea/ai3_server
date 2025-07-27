469c6da2ac036e8214d930967f1384015332a512
wonjunsea <jun040604@naver.com>
2025-07-27 20:38:16 +0900

    fix: set dynamic port for Render deployment

--- server.py
+++ server.py
@@ -38,7 +38,6 @@ def clova_summary():
             ),
             500,
         )
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 4000))
     app.run(host="0.0.0.0", port=port)
